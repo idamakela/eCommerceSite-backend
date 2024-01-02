73d680a5659d3f51ac23286238558eac1048fda5
idamakela <idamakela98@gmail.com>
2024-01-02 12:46:49 +0100

    init commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '12915d97674875cf041d63d3bc592791'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '03e35b89-49f1-459f-a948-ff8fea289a34'
+};
